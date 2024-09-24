6426afa2068361b6a3c4033413373dfd71a99b3a
lanaAh26 <ahlna2627@gmail.com>
2024-09-24 20:09:32 +0300

    مهمة عداد الساعة

--- app/src/main/java/com/example/no2/MainActivity.java
+++ app/src/main/java/com/example/no2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.no2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,76 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int min, sec;
+    TextView tv;
+    public Thread counterThread;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        tv=findViewById(R.id.text);
+        sec = 0;
+        min = 0;
+
+
+    }
+
+    public void startCount(View view) {
+        counterThread = new Thread(() -> {
+            try {
+                while (true) {
+                    if (sec == 59) {
+                        sec = 0;
+                        min++;
+                        if(min>=10&&sec>=10){
+                            tv.setText(min+":"+sec);
+                        }
+                        if(min<10&&sec>=10){
+                            tv.setText("0"+min+":"+sec);
+                        }
+                        if(min<10&&sec<10){
+                            tv.setText("0"+min+":"+"0"+sec);
+                        }
+                        if(min>=10&&sec<10){
+                            tv.setText(min+":"+"0"+sec);
+                        }
+                        Thread.sleep(1000);
+                    } else {
+                        sec++;
+                        if(min>=10&&sec>=10){
+                            tv.setText(min+":"+sec);
+                        }
+                        if(min<10&&sec>=10){
+                            tv.setText("0"+min+":"+sec);
+                        }
+                        if(min<10&&sec<10){
+                            tv.setText("0"+min+":"+"0"+sec);
+                        }
+                        if(min>=10&&sec<10){
+                            tv.setText(min+":"+"0"+sec);
+                        }
+                        Thread.sleep(1000);
+                    }
+                }
+            } catch (Exception e) {
+
+            }
         });
+        counterThread.start();
+    }
+
+    public void stop(View view) {
+        counterThread.interrupt();
+    }
+
+    public void reset(View view){
+        counterThread.interrupt();
+        min=0;
+        sec=0;
+        tv.setText("00:00");
+
+
     }
 }
